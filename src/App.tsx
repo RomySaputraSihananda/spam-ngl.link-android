595ac225c8177f0ae561ee9b339018cb075ed475
romys <romysaputrasihanandaa@gmail.com>
2023-11-13 00:41:17 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,34 @@ import Search from './views/Seacrh';
 import styles from './styles/styles';
 import Logger from './views/Logger';
 
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+// function App() {
+//   return (
+// <NavigationContainer>
+//   <Stack.Navigator>
+//     <Stack.Screen name="Home" component={HomeScreen} />
+//   </Stack.Navigator>
+// </NavigationContainer>
+//   );
+// }
+
+// export default App;
 class App extends React.Component {
   render = () => {
     return (
       <View style={styles.body}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={Search} />
+            <Stack.Screen name="logger" component={Logger} />
+          </Stack.Navigator>
+        </NavigationContainer>
         {/* <Search /> */}
-        <Logger />
+        {/* <Logger /> */}
       </View>
     );
   };
